30a5f94c727cae9760945d81f6c12619f6629745
nanonixx <nhid@elpuig.xeill.net>
2020-09-30 20:30:17 +0200

    Initial commit

---
+++ app/src/main/java/com/company/onboarding/Onboarding1Fragment.java
@@ -0,0 +1,50 @@
+package com.company.onboarding;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the  factory method to
+ * create an instance of this fragment.
+ */
+public class Onboarding1Fragment extends Fragment {
+
+    Button botonSiguiente;
+    NavController navController;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding1, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+
+        botonSiguiente = view.findViewById(R.id.botonSiguiente);
+
+        botonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_onboarding1Fragment_to_onboarding2Fragment);
+            }
+        });
+    }
+
+}
